5668be79bef156ed822458a665a12bb2e8c37658
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-08-29 12:26:14 -0300

    Implements Srping WebSecurity configuration

--- src/main/java/com/devsuperior/movieflix/services/UserService.java
+++ src/main/java/com/devsuperior/movieflix/services/UserService.java
@@ -6,6 +6,9 @@ import com.devsuperior.movieflix.entities.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,22 +16,37 @@ import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private AuthService authService;
+
     @Transactional(readOnly = true)
     public UserDTO findById(Long id) {
-/*
+
         authService.validateSelfOrAdmin(id);
-*/
+
             Optional<User> obj = userRepository.findById(id);
             User user = obj.orElseThrow(() -> new EntityNotFoundException("User not found with id " + id));
             return new UserDTO(user);
 
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(s);
+        if (user == null) {
+            logger.error("user not found " + s);
+            throw new UsernameNotFoundException("Email não encontrado");
+        }
+        logger.info("User found " + s);
+        return user;
     }
 
 
