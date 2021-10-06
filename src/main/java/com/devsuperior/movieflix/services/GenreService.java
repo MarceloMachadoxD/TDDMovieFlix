db57e7f704478f256400a2fafb0a869db7e2989e
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-10-06 20:35:34 -0300

    Solve GenreControllerIT tests

---
+++ src/main/java/com/devsuperior/movieflix/services/GenreService.java
@@ -0,0 +1,38 @@
+package com.devsuperior.movieflix.services;
+
+import com.devsuperior.movieflix.Repositories.GenreRepository;
+import com.devsuperior.movieflix.Repositories.UserRepository;
+import com.devsuperior.movieflix.entities.DTO.GenreDTO;
+import com.devsuperior.movieflix.entities.DTO.UserDTO;
+import com.devsuperior.movieflix.entities.Genre;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class GenreService {
+
+    private static Logger logger = LoggerFactory.getLogger(UserService.class);
+
+    @Autowired
+    private GenreRepository genreRepository;
+
+    @Autowired
+    private AuthService authService;
+
+
+    @Transactional(readOnly = true)
+    public List<GenreDTO> findAll() {
+       List<Genre> list = genreRepository.findAll();
+       List<GenreDTO> listDTO =  list.stream().map(x -> new GenreDTO(x)).collect(Collectors.toList());
+       return listDTO;
+    }
+
+
+
+}
