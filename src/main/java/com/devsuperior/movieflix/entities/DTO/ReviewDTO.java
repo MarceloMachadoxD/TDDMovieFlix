30102cdc17f70934335d16f6162602891ba7b167
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-10-06 19:56:10 -0300

    Ajdust mininal entities to build project

--- src/main/java/com/devsuperior/movieflix/entities/DTO/ReviewDTO.java
+++ src/main/java/com/devsuperior/movieflix/entities/DTO/ReviewDTO.java
@@ -1,4 +1,61 @@
 package com.devsuperior.movieflix.entities.DTO;
 
+import com.devsuperior.movieflix.entities.Review;
+
 public class ReviewDTO {
+
+    private Long id;
+    private String text;
+
+    private Long movieId;
+    private Long userId;
+
+
+    public ReviewDTO() {
+    }
+
+    public ReviewDTO(Long id, String text) {
+        this.id = id;
+        this.text = text;
+    }
+
+    public ReviewDTO(Review entity) {
+
+        this.id = entity.getId();
+        this.text = entity.getText();
+        this.movieId = entity.getMovie().getId();
+        this.userId = entity.getUser().getId();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Long getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(Long movieId) {
+        this.movieId = movieId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
 }
